13f8d39ec4627b85cabb95c724b6387cc4c1831e
Tamene Gelaye <tamenes21@gmail.com>
2023-08-13 13:28:49 +0000

    request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -52,11 +52,9 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
 
     current_user = auth.current_user(request)
     if current_user is None:
