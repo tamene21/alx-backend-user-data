4903d6d18b9a50c0ebdc87384d2a43f3dfef2ea7
Tamene Gelaye <tamenes21@gmail.com>
2023-08-15 08:51:47 +0000

    code style

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -18,3 +18,22 @@ def _hash_password(password: str) -> str:
     Returns bytes (salted_hashed)
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register new user
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists.'.format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
