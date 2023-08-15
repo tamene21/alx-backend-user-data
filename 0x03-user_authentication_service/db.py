d72fa9c32ef9af50ac40c4d577aa7347c5a5139a
Tamene Gelaye <tamenes21@gmail.com>
2023-08-15 06:49:39 +0000

    data type

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,7 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email=str, hashed_password=str) -> User:
+    def add_user(self, email:str, hashed_password:str) -> User:
         """Add and return a new user
         """
         user = User(email=email, hashed_password=hashed_password)
