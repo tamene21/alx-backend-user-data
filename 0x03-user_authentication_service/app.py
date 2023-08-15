bbbb6eedb1d8b7a1d2e95c5ec4c07cde72531df8
Tamene Gelaye <tamenes21@gmail.com>
2023-08-15 09:49:54 +0000

    register a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -21,5 +21,19 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """POST /users, JSON: -email, -password
+    Returns end-point
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
