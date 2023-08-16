bc8320f01764cda1879bac06b5949c58d0e21910
Tamene Gelaye <tamenes21@gmail.com>
2023-08-16 06:56:41 +0000

    loging in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,5 +35,28 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/session', mthods=['POST'], strict_slashes=False)
+def login() -> str:
+    """Logs in a user and return session id
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    if not Auth.valid_login(email, password):
+        abort(401)
+
+    session_id = Auth.create_session(email)
+
+    msg = ['email': email, 'message': 'loged in']
+    response = jsonify(msg)
+
+    response.set_cookie("session_id", session_id)
+
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
