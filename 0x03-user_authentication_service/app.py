c7213df2f60964c1d0693d3376f84c161233fe4e
Tamene Gelaye <tamenes21@gmail.com>
2023-08-16 07:13:20 +0000

    route method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -35,7 +35,7 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', mthods=['POST'], strict_slashes=False)
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
     """Logs in a user and return session id
     """
