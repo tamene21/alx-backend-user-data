95bad1d1a4239bc0751b6ade264e82f2ff0191e2
Tamene Gelaye <tamenes21@gmail.com>
2023-08-18 11:14:36 +0000

    update_request route

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -47,15 +47,21 @@ def before_request() -> str:
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+    if auth:
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(
+                    request) is None and auth.session_cookie(request) is None:
+                abort(401)
+            request.current_user = auth.current_user(request)
+            if auth.current_user(request) is None:
+                abort(403)
+            if request.current_user is None:
+                abort(403)
 
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-   request.current_user =  auth.current_user(request)
+                      
+            
 
 
 if __name__ == "__main__":
